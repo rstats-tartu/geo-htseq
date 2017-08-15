88998a40e3b0af890c53b274a4708c21147870ed
Taavi Päll <tapa741@gmail.com>
2017-08-15 18:03:10 +0300

    query string and retmax

--- src/A01_GEO_query.R
+++ src/A01_GEO_query.R
@@ -1,27 +1,22 @@
 
 # Run GEO query for HT sequencing data --------------------------------------------------------
-library(tidyverse)
-library(magrittr)
+library(dplyr)
 
 # @knitr query-string
-query <- 'expression profiling by high throughput sequencing[DataSet Type] AND (Homo sapiens[Organism] OR Mus musculus[Organism])'
+query <- 'expression profiling by high throughput sequencing[DataSet Type]'
 
 # @knitr run-query
 # Load helper functions
 source("lib/get_GEO_funs.R")
 
 # get Ids
-Ids <- get_ids(query, retmax = 13000)
+Ids <- get_ids(query, retmax = 15000)
 
 # Get query summaries -----------------------------------------------------
 sumcont <- get_docsums(Ids)
 
-# XML stuff ------------------------------------------------------
-
-# extract docsums data much much faster
-# library(XML)
-
 # Gateway error in previous step may fuck up this step!!!, if this occurs, please just rerun.
-ds <- sumcont %>% lapply(extract_docsums) %>% bind_rows()
+ds <- lapply(sumcont, extract_docsums) %>% bind_rows()
+
 # ds is short of document summaries
 save(ds, file = "data/ds.RData")
