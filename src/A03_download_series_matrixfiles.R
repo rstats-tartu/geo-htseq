51a2dce2907e070d5c4f7bbece8eb7ebaf5b837a
Taavi Päll <tapa741@gmail.com>
2017-08-16 15:14:00 +0300

    rename

--- src/A03_download_series_matrixfiles.R
+++ src/A03_download_series_matrixfiles.R
@@ -1,7 +1,6 @@
 
-library(tidyverse)
+library(dplyr)
 library(lubridate)
-library(stringr)
 library(GEOquery)
 
 newquery <- FALSE
@@ -27,30 +26,17 @@ suppfiles_acc <- data_frame(SuppFileNames = list.files(local_suppfile_folder)) %
   mutate(Accession = str_extract(SuppFileNames, "GSE[[:digit:]]*")) %>% 
   nest(SuppFileNames)
 
-# Download missing series matrix files
+# Missing series matrix files
 gsematrix <- ds %>% 
   mutate_at("PDAT", ymd) %>% 
-  filter(PDAT<=ymd("2017-06-19")) %>%
+  filter(PDAT <= ymd("2017-06-19"),
+         str_detect(taxon, "Mus musculus|Homo sapiens")) %>%
   select(Accession, PDAT) %>% 
   distinct %>% 
   anti_join(matrixfiles_acc) 
 
-# supplemental file GEOs missing series matrix file
-gsematrix <- mutate(gsematrix, gsem = map(Accession, ~ try(getGEO(GEO = .x, 
-                                                                  destdir = local_matrixfile_folder, 
-                                                                  getGPL = FALSE))))
-
-# Missing series matrix files are behind password
-missing_series_matrix <- gsematrix %>% 
-  filter(map_lgl(gsem, ~inherits(.x, "try-error")))
-
-missing_series_matrix %>% 
-  filter(!is.na(PDAT)) %>% 
-  count(PDAT) %>% 
-  mutate(n = cumsum(n),
-         PDAT = ymd(PDAT)) %>% 
-  ggplot(aes(PDAT, n, group = 1)) + 
-  geom_line() +
-  ggtitle("GEO series with missing series matrix file") +
-  ylab("Number of GEO series")
-
+# Download supplemental file GEOs missing series matrix file
+gsematrix <- mutate(gsematrix, 
+                    gsem = map(Accession, ~ try(getGEO(GEO = .x, 
+                                                       destdir = local_matrixfile_folder, 
+                                                       getGPL = FALSE))))
