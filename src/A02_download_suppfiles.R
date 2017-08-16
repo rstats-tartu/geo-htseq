99c38e68316b85c9049244361c8694ece756e121
Taavi Päll <tapa741@gmail.com>
2017-08-16 15:14:51 +0300

    removed unnecessay lines

--- src/A02_download_suppfiles.R
+++ src/A02_download_suppfiles.R
@@ -1,25 +1,43 @@
 
-source("src/A01_GEO_query.R")
+freshgeoquery <- FALSE
+if(freshgeoquery&&!file.exists("data/ds.RData")){
+  source("src/A01_GEO_query.R")
+} else{
+  load("data/ds.RData")
+}
+
 source("lib/getDirListing.R")
 library(tidyverse)
+library(lubridate)
 library(magrittr)
+library(formattable)
 library(stringr)
 library(GEOquery)
 
 # Retrieve filenames 
-start <- Sys.time()
-suppfilenames <- ds %>%
-  mutate(SuppFileNames = map(FTPLink, ~try(getDirListing(file.path(.x, "suppl/")))))
-end <- Sys.time()
-save(suppfilenames, file = "data/suppfilenames_2017-06-19.RData")
-start-end 
-# Time difference of -7 hours
+fullsuppfilenamesquery <- FALSE
+if(fullsuppfilenamesquery){
+  # getting directory listings for complete dataset may take long time: ~7 hours
+  suppfilenames <- ds %>%
+    filter(ymd(PDAT) <= last_date, 
+           str_detect(taxon, "Mus musculus|Homo sapiens")) %>% 
+    mutate(SuppFileNames = map(FTPLink, ~try(getDirListing(file.path(.x, "suppl/")))))
+  save(suppfilenames, file = "data/suppfilenames_2017-06-19.RData")
+} else {
+  load("data/suppfilenames_2017-06-19.RData")
+}
 
+# Accessions from query missing from suppfilenames data.frame
+missing_suppfilenames <- filter(ds, ymd(PDAT) <= last_date, 
+                           str_detect(taxon, "Mus musculus|Homo sapiens"),
+                           !(Accession %in% suppfilenames$Accession))
+missing_suppfilenames <- mutate(missing_suppfilenames, SuppFileNames = map(FTPLink, ~try(getDirListing(file.path(.x, "suppl/")))))
+suppfilenames <- bind_rows(suppfilenames, missing_suppfilenames)
 
 # Files with name extentions that we are NOT going to download and --------
 
 ## @knitr out_strings
-out_string1 <- c("filelist|annotation|readme|error|raw.tar|csfasta|bam|sam|bed|[:punct:]hic|hdf5|bismark")
+out_string1 <- c("filelist|annotation|readme|error|raw.tar|csfasta|bam|sam|bed|[:punct:]hic|hdf5|bismark|map|barcode|peaks")
 out_string2 <- c("tar","gtf","(big)?bed(\\.txt|12|graph|pk)?","bw","wig","hic","gct(x)?","tdf",
          "gff(3)?","pdf","png","zip","sif","narrowpeak","fa")
 out_strings <- unique(c(unlist(str_split(out_string1, "\\|")), out_string2))
@@ -34,24 +52,17 @@ suppfiles_of_interest <- suppfilenames %>%
   select(Accession, SuppFileNames, FTPLink, PDAT) %>% 
   mutate(filext = str_extract(tolower(SuppFileNames), "\\.[:alpha:]+([:punct:][bgz2]+)?$")) 
 
-suppf_oi_perc <- percent(1-(n_distinct(suppfiles_of_interest$Accession)/n_distinct(suppfilenames$Accession)))
-
 ## @knitr suppfile-download-code-n
 # Folder where downloaded supplementary files are stored
 local_suppfile_folder <- "/Volumes/Media/srp_example/data" # "data/counts"
 
-# Download if suppfile is missing, some big files are moved into separate folder
+# Download if suppfile is missing, some big files are moved into separate folder, hence recursive = T
 local_files <- list.files(local_suppfile_folder, recursive = T) %>% 
   str_replace(".*\\/","")
-
-data_frame(local_files) %>% 
-  filter(str_detect(tolower(local_files), "peak")) %$% 
-  local_files
 
 missing_local <- suppfiles_of_interest %>% 
   filter(!str_replace(tolower(SuppFileNames),"\\.(gz|bz2)$","") %in% 
            str_replace(tolower(local_files), "\\.(gz|bz2)$",""))
 
 # Download supplementary files --------------------------------------------
-missing_local %>% 
-  mutate(map2(FTPLink, SuppFileNames, ~ geo_supp_dwnl(.x, .y, local_suppfile_folder)))
+mutate(missing_local, map2(FTPLink, SuppFileNames, ~ geo_supp_dwnl(.x, .y, local_suppfile_folder)))
