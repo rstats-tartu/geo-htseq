e6280e041d3a152edd768402ed177fc12312779d
Taavi Päll <tapa741@gmail.com>
2017-08-17 22:01:23 +0300

    use entrezquery package

--- src/A01_GEO_query.R
+++ src/A01_GEO_query.R
@@ -1,22 +1,15 @@
 
 # Run GEO query for HT sequencing data --------------------------------------------------------
-library(dplyr)
+## Library can be installed from github
+# devtools::install_github("tpall/entrezquery")
+library(entrezquery)
 
 # @knitr query-string
 query <- 'expression profiling by high throughput sequencing[DataSet Type]'
 
 # @knitr run-query
-# Load helper functions
-source("lib/get_GEO_funs.R")
-
-# get Ids
-Ids <- get_ids(query, retmax = 15000)
-
-# Get query summaries -----------------------------------------------------
-sumcont <- get_docsums(Ids)
-
-# Gateway error in previous step may fuck up this step!!!, if this occurs, please just rerun.
-ds <- lapply(sumcont, extract_docsums) %>% bind_rows()
+ds <- entrez_docsums(query = query, db = "gds", retmax = 15000)
 
 # ds is short of document summaries
 save(ds, file = "data/ds.RData")
+
