aa75c51dcff11e569d2a799f6f52598ae0a40354
Taavi Päll <tapa741@gmail.com>
2017-08-24 17:58:37 +0300

    updated get_pvalues and munge_geo2 functions

--- lib/munge_geo.R
+++ lib/munge_geo.R
@@ -75,54 +75,78 @@ read_geotabs <- function(path){
   return(tab)
 }
 
-
 # get_pvalues_basemean ----------------------------------------------------
-#' @import magrittr
+#' Extract p value and when available basemean columns from geo data frames.
+#' @description Function tries to identify and subscript raw p value and base mean columns from data frame. When multiple basemean columns are detected, function calculates rowmeans.
+#' @param x data frame imported from Entrez GEO supplementary file.
+#' @return Data frame with pvalue columns and basemean column.
+#' @examples \notrun {
+#' library(data.table)
+#' ## Dataset misses base mean column
+#' x <- fread(sprintf("zcat < %s", "inst/extdata/GSE66793_cmp1-3-geo-gene.tsv.gz"))
+#' pv <- get_pvalues_basemean(as.data.frame(x))
+#' }
+#' @import dplyr
 #' @import stringr
-get_pvalues_basemean <- function(restab){
+get_pvalues_basemean <- function(x){
   
-  if(inherits(restab, "try-error")){
-    stop("No table to look into!")
+  ## Remove NA and duplicated columns
+  colns <- colnames(x)
+  colns <- colns[!is.na(colns)] 
+  colns <- colns[!duplicated(colns)]
+  
+  ## If matrix convert to data.frame
+  if(is.matrix(x)){
+    x <- as.data.frame(x)
   }
   
-  rescols <- colnames(restab) %>% .[!is.na(.)] %>% .[!duplicated(.)]
-  
-  if(is.matrix(restab)){
-    restab %<>% as.data.frame
+  ## Fix colnames 
+  if(str_detect(tail(colns, 1), "V[0-9]")){
+    colnames(x) <- c(tail(colns, 1), setdiff(colns, tail(colns, 1)))
   }
   
-  # Fix colnames
-  if(str_detect(tail(rescols, 1), "V[0-9]")){
-    colnames(restab) <- c(tail(rescols, 1), setdiff(rescols, tail(rescols, 1)))
-  }
+  x <-  x[colns]
+  pval_col <- str_detect(colns, "^p(-)?val") & !str_detect(colns, "[Aa]dj|FDR|Corrected")
   
-  restab %<>% "["(rescols)
-  restab <- restab[str_detect(colnames(restab), "base[Mm]ean|^p(-)?val") & !str_detect(colnames(restab), "[Aa]dj|FDR|Corrected")]
-  
-  ## Check if p values are in the 0 to 1 range
-  pvals_ok <- vapply(restab, function(x) {all(x>=0 & x<=1)}, logical(1))
-  bm <- str_detect(colnames(restab), "base[Mm]ean")
-  restab <- restab[mapply(any, pvals_ok, bm)]
-  
-  if(ncol(restab) == 0 || !any(str_detect(colnames(restab), "p(-)?val"))){
-    return(NULL)
-  }
-  
-  if(any(str_detect(colnames(restab), "base[Mm]ean"))) {
-    restab %<>% 
-      mutate(bmean = rowMeans(select(., matches("basemean"))))
-  }
-  
-  restab %<>% select(matches("bmean|p(-)?val"))
-  colnames(restab)[str_detect(colnames(restab),"p(-)?val")] <- "pvalue"
-  
-  if(any(str_detect(colnames(restab), "bmean"))){
-    colnames(restab)[str_detect(colnames(restab),"bmean")] <- "basemean"
+  ## If P value column present, check if P values are between 0 and 1 
+  if(any(pval_col)){
+    
+    pvals_ok <- vapply(x[pval_col], function(x) {all(na.omit(x)>=0 & na.omit(x)<=1)}, logical(1))
+    
+    if(!all(pvals_ok)){
+      
+      stop("Detected P values not in 0 to 1 range or otherwise malformed!")
+      
+    }
+    
   } else {
-    restab$basemean <- NA
+    
+    return(NULL) 
+    
   }
   
-  return(restab)
+  basemean_col <- str_detect(colns, "base[Mm]ean")
+  # restab <- restab[mapply(any, pval_col, basemean_col)]
+  
+  ## Summarise multiple basemean columns
+  if(any(basemean_col)) {
+    x <- mutate(x, bmean = rowMeans(select(x, matches("basemean"))))
+  }
+  
+  ## Select only basemean and pvalue olumns
+  x <- select(x, matches("bmean|p(-)?val"))
+  
+  ## Rename pvalue column
+  colnames(x)[str_detect(colnames(x),"p(-)?val")] <- "pvalue"
+  
+  ## Rename basemean column
+  if(any(str_detect(colnames(x), "bmean"))){
+    colnames(x)[str_detect(colnames(x),"bmean")] <- "basemean"
+  } else {
+    x$basemean <- NA
+  }
+  
+  return(x)
 }
 
 # munge_geo function ------------------------------------------------------
@@ -230,3 +254,35 @@ munge_geo <- function(eset, countfile, dir = ".") {
   return(c(pvalues, neweset))
 }
 
+munge_geo2 <- function(countfile, dir = ".") {
+  
+  ## Assemble path to supplementary file
+  path <- file.path(dir, countfile)
+  
+  ## Import supplemental file, 
+  supptab <- read_geotabs(path)
+  
+  if(inherits(supptab, "try-error")){
+    stop("Table import error!")
+  }
+  
+  ## Convert to list if single table
+  if(is.data.frame(supptab)|is.matrix(supptab)){
+    supptab <- list(supptab) 
+  }
+  
+  ## Check for pvalues
+  pvalues <- map(supptab, ~try(get_pvalues_basemean(.x)))
+  heads <- map(supptab, head)
+  features <- map_int(supptab, nrow)
+  columns <- map_int(supptab, ncol)
+  supptab_names <- names(supptab)
+  
+  if(length(supptab_names)!=0){
+    sheets <- as.list(supptab_names)
+  } else {
+    sheets <- list("")
+  }
+  
+  data_frame(sheets, heads, pvalues, features, columns)
+}
