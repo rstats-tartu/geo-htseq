b67a1073834fbc65d56e2e8a6582d91460e258dd
Taavi Päll <tapa741@gmail.com>
2017-08-25 23:16:49 +0300

    new tidy functions

--- lib/getDirListing.R
+++ lib/getDirListing.R
@@ -28,20 +28,73 @@ getGEOSuppFileNames <- function(ftplink){
     getDirListing(url)
     , silent = TRUE)
   if (inherits(fnames, "try-error")) {
-    warning("No supplemental files found. Check URL manually if in doubt.")
-    message(url)
+    message(sprintf("No files found in %s.\nCheck URL manually if in doubt", url))
     return()
   }
   return(fnames)
 }
 
 
+# get dir listing tidy ----------------------------------------------------
+
+#' Return current year when clock time is provided from entrez GEO ftp.
+#' @description Entrez database shows clock time for files submitted on the current year.
+#' @param x character string of clock time e.g. '6:21' or year.
+#' @return character string with current year.
+this_year <- function(x){
+  if(str_detect(x, ":")) {
+    x <- format(Sys.Date(), "%Y")
+  }
+  return(x)
+}
+
+#' Returns supplementary file names from Entrez GEO.
+#' @description Replaces GEOquery packages getDirlisting function, seems to be faster.
+#' @param r Object of class 'response'.
+#' @return A data frame with columns date, size and suppfile. 
+#' Date is the supplementary file date, size is filesize in bytes. 
+#' Suppfile is the supplementary file name.
+#' @examples \notrun{
+#' 
+#' ## Download supplementary file name
+#' url <-  "ftp://ftp.ncbi.nlm.nih.gov/geo/series/GSE81nnn/GSE81555//suppl/"
+#' r <- httr::GET(url)
+#' suppfiles <- get_dirlist(r)
+#' 
+#' ## Download series matrix file name
+#' url <-  "ftp://ftp.ncbi.nlm.nih.gov/geo/series/GSE81nnn/GSE81555//miniml/"
+#' r <- httr::GET(url)
+#' matrixfile <- get_dirlist(r)
+#' 
+#' }
+#' @import dplyr
+#' @import httr
+#' @export
+get_dirlist <- function(r){
+  
+  if(class(r) != "response") {
+    warning("Not html response!")
+    return()
+  }
+  
+  cont <- content(r, as = "text", encoding = "UTF-8")
+  tb <- readr::read_delim(cont, "\n", col_names = F)
+  tb <- tidyr::separate(tb, "X1", paste0("C", 1:9), "[[:space:]]+")
+  tb <- dplyr::mutate(tb, year = purrr::map_chr(C8, this_year),
+                      month = which(stringr::str_detect(month.abb, C6)),
+                      date = lubridate::dmy(paste(C7, month, year, sep = "-")))
+  tb <- dplyr::select(tb, date, C5, C9)
+  colnames(tb) <- c("date", "size", str_extract(r$url, "miniml|suppl"))
+  return(tb)
+}
+
 # download supplementary files --------------------------------------------
 
-geo_supp_dwnl <- function(ftplink, suppfile, destdir) {
+geo_supp_dwnl <- function(ftplink, slug = c("suppl", "miniml"), suppfile, destdir) {
+  
   message(suppfile)
   
-  fp <- file.path(ftplink, "suppl", suppfile)
+  fp <- file.path(ftplink, slug, suppfile)
   dest <- file.path(destdir, suppfile)
   
   if(file.exists(dest)){
@@ -62,9 +115,7 @@ getGEOSeriesFiles <- function(ftplink, subdir = "miniml/"){
   url <- file.path(ftplink, subdir)
   fname <- try(getDirListing(url), silent = TRUE)
   if (inherits(fname, "try-error")) {
-    message("No files found.")
-    message("Check URL manually if in doubt")
-    message(url)
+    message(sprintf("No files found in %s.\nCheck URL manually if in doubt", url))
     return(NULL)
   }
 
