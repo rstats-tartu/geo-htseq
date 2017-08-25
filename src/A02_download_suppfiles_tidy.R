101d3169a7c885ffad1478ebb6269e4c90330ffc
Taavi Päll <tapa741@gmail.com>
2017-08-25 23:16:21 +0300

    httr based function
    faster!

---
+++ src/A02_download_suppfiles_tidy.R
@@ -0,0 +1,28 @@
+
+library(dplyr)
+library(purrr)
+library(tidyr)
+library(httr)
+library(lubridate)
+library(stringr)
+source("lib/getDirListing.R")
+load("data/ds.RData")
+
+ftplinks <- filter(ds, ymd(PDAT) <= "2017-06-19", 
+                   str_detect(taxon, "Mus musculus|Homo sapiens"))
+
+
+which(ftplinks$Accession=="GSE9306")
+
+start <- Sys.time()
+suppfilenames <- mutate(ftplinks, url = file.path(FTPLink, "suppl/")) %>% 
+  sample_n(100) %>% 
+  mutate(r = map(url, ~{message(.x); try(httr::GET(.x))}))
+end <- Sys.time()
+end-start
+cat(sprintf("Downloading filenames took %s", end-start), file = "suppfilename.log", append = T)
+
+save(suppfilenames, file = "data/suppfilenames_2017-08-25.RData")
+suppfilenames  <- filter(suppfilenames, !map_lgl(r, inherits, "try-error")) %>% 
+  mutate(suppfiles = map(r, get_dirlist))
+# save(suppfilenames, file = "data/suppfilenames_2017-08-25.RData")
