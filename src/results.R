80562204f8ec41f7708b8c0a882ebe7464dc3bac
Taavi Päll <tapa741@gmail.com>
2017-08-31 15:57:54 +0300

    a->o

--- src/results.R
+++ src/results.R
@@ -164,7 +164,7 @@ supp_raw_perc <- percent(n_distinct(filter(cf, str_detect(common_filenames, "raw
 
 # Filter downloaded supplementary file names ------------------------------
 # In this section, we filter supplementary file names for patterns: 
-# we are looking anly for tabular data. 
+# we are looking only for tabular data. 
 out_string1 <- c("filelist|annotation|readme|error|raw.tar|csfasta|bam|sam|bed|[:punct:]hic|hdf5|bismark|map|barcode|peaks")
 out_string2 <- c("tar","gtf","(big)?bed(\\.txt|12|graph|pk)?","bw","wig","hic","gct(x)?","tdf",
                  "gff(3)?","pdf","png","zip","sif","narrowpeak","fa", "r$", "rda(ta)?$")
