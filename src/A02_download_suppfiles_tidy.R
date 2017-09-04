e542382c0a6d4aa4f8314ad49f476dabd602e504
Taavi Päll <tapa741@gmail.com>
2017-09-04 23:08:41 +0300

    load ds when missing and other formatting

--- src/A02_download_suppfiles_tidy.R
+++ src/A02_download_suppfiles_tidy.R
@@ -6,20 +6,22 @@ library(httr)
 library(lubridate)
 library(stringr)
 source("lib/getDirListing.R")
-load("data/ds.RData")
+if(!"ds" %in% ls()){
+  load("data/ds.RData")
+}
 
-ftplinks <- filter(ds, ymd(PDAT) <= "2017-06-19", 
-                   str_detect(taxon, "Mus musculus|Homo sapiens"))
+ds_filtered <- filter(ds, 
+                      ymd(PDAT) <= "2017-06-19", 
+                      str_detect(taxon, "Mus musculus|Homo sapiens"))
 
 start <- Sys.time()
-suppfilenames <- mutate(ftplinks, url = file.path(FTPLink, "suppl/")) %>% 
+suppfilenames <- mutate(ds_filtered, url = file.path(FTPLink, "suppl/")) %>% 
   sample_n(100) %>% 
   mutate(r = map(url, ~{message(.x); try(httr::GET(.x))}))
 end <- Sys.time()
 end-start
-cat(sprintf("Downloading filenames took %s", end-start), file = "suppfilename.log", append = T)
+cat(sprintf("Downloading filenames took %s hours\n", end-start), file = "suppfilename.log", append = T)
 
 save(suppfilenames, file = "data/suppfilenames_2017-08-25.RData")
 suppfilenames  <- filter(suppfilenames, !map_lgl(r, inherits, "try-error")) %>% 
   mutate(suppfiles = map(r, get_dirlist))
-
