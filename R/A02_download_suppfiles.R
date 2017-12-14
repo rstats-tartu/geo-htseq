2cc9c6df985360759f6caeaa5b4ede3517d8cebb
Taavi Päll <tapa741@gmail.com>
2017-12-14 15:50:10 +0200

    httr version

--- R/A02_download_suppfiles.R
+++ R/A02_download_suppfiles.R
@@ -2,19 +2,18 @@
 library(tidyverse)
 library(httr)
 library(lubridate)
-source("lib/getDirListing.R")
 
 if(!"ds" %in% ls()){
   ds <- readRDS("data/document_summaries.rds")
 }
 
-ds_filtered <- filter(ds, 
-                      ymd(PDAT) <= "2017-06-19", 
-                      str_detect(taxon, "Mus musculus|Homo sapiens"))
+ds_filtered <- ds %>% 
+  filter(ymd(PDAT) <= "2017-06-19", 
+         str_detect(taxon, "Mus musculus|Homo sapiens"))
 
 start <- Sys.time()
 suppfilenames <- mutate(ds_filtered, url = file.path(FTPLink, "suppl/")) %>% 
-  sample_n(10) %>% 
+  sample_n(10) %>%
   mutate(r = map(url, ~{message(.x); try(httr::GET(.x))}))
 end <- Sys.time()
 end-start
