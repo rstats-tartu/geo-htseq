61c6c6ae32371af7ac4b418307781a5e1504b019
Taavi Päll <tapa741@gmail.com>
2018-01-22 22:56:11 +0200

    added file to manually check publications for p values

---
+++ R/sample_publications.R
@@ -0,0 +1,17 @@
+
+if (!any(str_detect(ls(), "pubs$"))) {
+  source("R/results_publications.R")
+}
+
+set.seed(122)
+tocheck <- pubs %>%
+  filter(str_detect(model, "Human")) %>%
+  sample_frac(0.02)
+
+tocheckids <- tocheck %>% 
+  group_by(ISSN, Source) %>% 
+  nest() %>% 
+  arrange(desc(map_int(data, nrow))) %>% 
+  unnest() %>% 
+  select(ISSN, Source, DOI, Volume, Issue, Pages) %>% 
+  distinct()
