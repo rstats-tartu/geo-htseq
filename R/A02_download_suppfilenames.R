946037fa77df3f0f34b546c7c7fe142dd9692489
Taavi Päll <tapa741@gmail.com>
2018-03-01 15:32:59 +0000

    removed species from filter

--- R/A02_download_suppfilenames.R
+++ R/A02_download_suppfilenames.R
@@ -1,22 +1,27 @@
 
-library(tidyverse)
-library(httr)
-library(lubridate)
+# Load libs
+source("R/_common.R")
+library(entrezquery)
 
 if (!"ds" %in% ls()) {
   ds <- readRDS("output/document_summaries.rds")
 }
 
 ds_filtered <- ds %>% 
-  filter(ymd(PDAT) <= "2017-06-19", 
-         str_detect(taxon, "Mus musculus|Homo sapiens"))
+  filter(PDAT <= last_date)
 
 start <- Sys.time()
-suppfilenames <- mutate(ds_filtered, url = file.path(FTPLink, "suppl/")) %>% 
-  mutate(r = map(url, ~{message(.x); try(httr::GET(.x))}))
+get_safely <- possibly(get_dirlist, data_frame)
+ds_filtered <- ds_filtered %>%
+  sample_n(10) %>% 
+  mutate(dirlist = map(Accession, ~ {message(.x); get_safely(.x)}))
 end <- Sys.time()
-end-start
 cat(sprintf("Downloading filenames took %s hours\n", end-start), file = "suppfilename.log", append = T)
+
+safe_download <- possibly(~ download_gsefiles(.x, dest = "output"), "fail")
+ds_filtered <- ds_filtered %>% 
+  mutate(files = map(dirlist, "file"),
+         download = map(files, ~ {message(paste(.x, collapse = "\n")); safe_download(.x)}))
 
 saveRDS(suppfilenames, file = "output/suppfilenames.rds")
 
