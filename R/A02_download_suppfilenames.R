1ec0297407c729dfb5176471756c33cda8ca1d50
Taavi Päll <tapa741@gmail.com>
2018-03-01 15:44:02 +0000

    comments

--- R/A02_download_suppfilenames.R
+++ R/A02_download_suppfilenames.R
@@ -1,27 +1,23 @@
-
 # Load libs
 source("R/_common.R")
 library(entrezquery)
 
+# Check if document summary table is already loaded
 if (!"ds" %in% ls()) {
   ds <- readRDS("output/document_summaries.rds")
 }
 
+# Filter by data
 ds_filtered <- ds %>% 
   filter(PDAT <= last_date)
 
-start <- Sys.time()
+# Start download
 get_safely <- possibly(get_dirlist, data_frame)
-ds_filtered <- ds_filtered %>%
-  sample_n(10) %>% 
+start <- Sys.time()
+suppfilenames <- ds_filtered %>%
   mutate(dirlist = map(Accession, ~ {message(.x); get_safely(.x)}))
 end <- Sys.time()
 cat(sprintf("Downloading filenames took %s hours\n", end-start), file = "suppfilename.log", append = T)
 
-safe_download <- possibly(~ download_gsefiles(.x, dest = "output"), "fail")
-ds_filtered <- ds_filtered %>% 
-  mutate(files = map(dirlist, "file"),
-         download = map(files, ~ {message(paste(.x, collapse = "\n")); safe_download(.x)}))
-
+# Save downloaded dataset
 saveRDS(suppfilenames, file = "output/suppfilenames.rds")
-
