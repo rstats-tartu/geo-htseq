83657f94b325ab18c2183308f04469bcd77a994f
Taavi Päll <taavi74@ut.ee>
2018-03-02 22:23:04 +0200

    download suppfiles

--- R/A03_download_suppfiles.R
+++ R/A03_download_suppfiles.R
@@ -3,16 +3,39 @@
 source("R/_common.R")
 library(entrezquery)
 
+# Load supplementary file names
 suppfilenames <- readRDS("output/suppfilenames.rds")
 
+# Safe wrap download function 
 safe_download <- safely(~ download_gsefiles(.x, dest = "output"))
-out <- suppfilenames %>% 
+
+# Munge file list
+sfn <- suppfilenames %>% 
   group_by(Accession) %>%
-  mutate(result = map(dirlist, "result")),
-         files = map(result, "file"))
-out %>% 
+  mutate(result = map(dirlist, "result"),
+         files = map(result, "file"),
+         type = map(result, "type")) %>% 
   select(Accession, files) %>% 
-  unnest() %>% 
-  filter(!stringr::str_detect(files, "RAW.tar|filelist.txt")) %>% 
+  filter(map_chr(files, class) != "NULL") %>% 
+  unnest()
+
+# Load 
+source("R/out_strings.R")
+
+start <- Sys.time()
+sfn_filtered <- sfn %>%
+  filter(!str_detect(tolower(files), str_c(out_string1, collapse = "|")),
+         !str_detect(tolower(files), str_c(out_string2, "(\\.gz|\\.bz2)?$", 
+                                                   collapse = "|"))) 
+
+sfn_filtered %>%
   group_by(Accession) %>%
-  do(dwnl = safe_download(.$files))
+  do(safe_download(.$files))
+end <- Sys.time()
+
+# Send remainder
+msg <- sprintf("Hi Taavi!\nSupplementary files download took %s hours and ended 
+               at %s.\nBest regards,\nYour Computer.", 
+               end - start, Sys.time())
+cmd <- sprintf("echo '%s' | mail -s 'Downloading files finished!' tapa741@gmail.com", msg)
+system(cmd)
--- R/results_suppfilenames.R
+++ R/results_suppfilenames.R
@@ -162,12 +162,7 @@ supp_raw_perc <- percent(n_raw / nrow(suppfilenames), 0)
 
 # ---- out-strings ----
 
-out_string1 <- c("filelist","annotation","readme","error","raw.tar","csfasta",
-                 "bam","sam","bed","[:punct:]hic","hdf5","bismark","map",
-                 "barcode","peaks")
-out_string2 <- c("tar","gtf","(big)?bed(\\.txt|12|graph|pk)?","bw","wig",
-                 "hic","gct(x)?","tdf","gff(3)?","pdf","png","zip","sif",
-                 "narrowpeak","fa", "r$", "rda(ta)?$")
+source("R/out_strings.R")
 
 # ---- filesofinterest ----
 suppfiles_of_interest <- suppfilenames_present_unnested %>%
