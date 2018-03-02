bd0509ead4fea3bbce050e28064512cbf83c9fd7
Taavi Päll <tapa741@gmail.com>
2018-03-02 08:19:15 +0000

    renamed files

---
+++ R/A03_download_suppfiles.R
@@ -0,0 +1,18 @@
+library(dplyr)
+library(purrr)
+library(entrezquery)
+set.seed(124)
+
+suppfilenames <- readRDS("output/suppfilenames.rds")
+
+safe_download <- safely(~ download_gsefiles(.x, dest = "output"))
+out <- suppfilenames %>% 
+  group_by(Accession) %>%
+  mutate(result = map(dirlist, "result")),
+         files = map(result, "file"))
+out %>% 
+  select(Accession, files) %>% 
+  unnest() %>% 
+  filter(!stringr::str_detect(files, "RAW.tar|filelist.txt")) %>% 
+  group_by(Accession) %>%
+  do(dwnl = safe_download(.$files))
