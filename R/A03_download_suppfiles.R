27db66cbdc2cb0e1bec07bb253764064fe838f5a
Taavi Päll <taavi74@ut.ee>
2018-03-03 14:15:48 +0200

    ok lets try one-by-one

--- R/A03_download_suppfiles.R
+++ R/A03_download_suppfiles.R
@@ -1,15 +1,15 @@
 
 # Load libs
+message("Loading libs\n")
 source("R/_common.R")
 library(entrezquery)
 
 # Load supplementary file names
+message("Loading supplementary file names\n")
 suppfilenames <- readRDS("output/suppfilenames.rds")
 
-# Safe wrap download function 
-safe_download <- safely(~ download_gsefiles(.x, dest = "output"))
-
 # Munge file list
+message("Munging file list\n")
 sfn <- suppfilenames %>% 
   group_by(Accession) %>%
   mutate(result = map(dirlist, "result"),
@@ -19,23 +19,32 @@ sfn <- suppfilenames %>%
   filter(map_chr(files, class) != "NULL") %>% 
   unnest()
 
-# Load 
+# Load out strings
+message("Loading out strings\n")
 source("R/out_strings.R")
 
-start <- Sys.time()
-sfn_filtered <- sfn %>%
+# Filter files of interest
+message("Filtering files of interest\n")
+sfn <- sfn %>%
   filter(!str_detect(tolower(files), str_c(out_string1, collapse = "|")),
          !str_detect(tolower(files), str_c(out_string2, "(\\.gz|\\.bz2)?$", 
                                                    collapse = "|"))) 
 
-sfn_filtered %>%
-  group_by(Accession) %>%
-  do(safe_download(.$files))
+# Safe wrap download function
+safe_download <- safely(~ download_gsefile(.x, dest = "output"))
+
+# Start download
+message("Sownloading files\n")
+start <- Sys.time()
+sfn %>% 
+  ungroup() %>% 
+  mutate(d = map(files, safe_download))
 end <- Sys.time()
 
-# Send remainder
-msg <- sprintf("Hi Taavi!\nSupplementary files download took %s hours and ended 
-               at %s.\nBest regards,\nYour Computer.", 
-               end - start, Sys.time())
-cmd <- sprintf("echo '%s' | mail -s 'Downloading files finished!' tapa741@gmail.com", msg)
+# Job finished, send email
+message("Job finished, sending email\n")
+msg <- sprintf("Subject:Downloading files finished!\n\nHi Taavi!\n\nSupplementary files download took %s and ended at %s.\n\nBest regards,\nYour Computer.", 
+               format(difftime(end, start)), Sys.time())
+cmd <- sprintf("echo -e '%s' | sendmail tapa741@gmail.com", msg)
 system(cmd)
+message("End")
