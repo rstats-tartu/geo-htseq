3d84c75b52242da68766360a2e8023dd838bae32
Taavi Päll <taavi74@ut.ee>
2018-03-15 23:30:30 +0200

    something's wrong

--- R/make-Makefile.R
+++ R/make-Makefile.R
@@ -1,9 +1,14 @@
-
+library(dplyr)
 glue::glue('
-{readr::read_lines("output/suppfilenames_filtered.txt")}: R/_common.R output/suppfilenames.rds R/out_strings.R\n
+#!/bin/bash\n
+output/suppdata.rds: R/_common.R output/document_summaries.rds output/gsem.rds lib/munge_geo.R lib/checkFullRank.R lib/text_funs.R\n
+\tRscript R/A06_munge_suppfiles.R\n
+output/gsem.rds: R/_common.R\n
+\tRscript R/A05_munge_series_matrixfiles.R\n
+{readr::read_lines("output/suppfilenames_filtered.txt")}: output/suppfilenames_filtered.rds\n
 \tRscript R/A04_download_suppfiles.R\n
-output/supfilenames_filtered.rds output/supfilenames_filtered.txt: output/suppfilenames.rds
-\tR/A03_filter_suppfilenames.R\n
+output/supfilenames_filtered.rds output/supfilenames_filtered.txt: R/_common.R output/suppfilenames.rds R/out_strings.R\n
+\tRscript R/A03_filter_suppfilenames.R\n
 output/suppfilenames.rds: output/document_summaries.rds R/A02_download_suppfilenames.R R/_common.R\n
 \tRscript R/A02_download_suppfilenames.R\n
 output/document_summaries.rds: R/A01_GEO_query.R\n
