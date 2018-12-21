ab891d1267fc7ff784bcc12dbed270e5e92af97d
Taavi Päll <taavi74@ut.ee>
2018-12-21 10:14:43 +0200

    fixed series matrix import

--- R/05_series_matrixfiles.R
+++ R/05_series_matrixfiles.R
@@ -5,16 +5,17 @@ library(GEOquery)
 munge_matrixfiles <- function(out_path) {
   # Munge matrixfiles -------------------------------------------------------
   # local_matrixfile_folder and local_suppfile_folder come from _common.R
+  acc <- "GSE[[:digit:]]+"
   matrixfiles <- local_matrixfile_folder %>% 
-    dir(pattern = "GSE[[:digit:]]*", full.names = TRUE) %>% 
+    dir(pattern = acc, full.names = TRUE) %>% 
     data_frame(series_matrix_file = .) %>% 
-    mutate(Accession = str_extract(toupper(series_matrix_file), "GSE[[:digit:]]*")) %>% 
+    mutate(Accession = str_extract(toupper(series_matrix_file), acc)) %>% 
     select(Accession, everything())
   
   suppfiles <- local_suppfile_folder %>% 
-    dir(pattern = "GSE[[:digit:]]*", full.names = TRUE) %>% 
+    dir(pattern = acc, full.names = TRUE) %>% 
     data_frame(files = .) %>% 
-    mutate(Accession = str_extract(toupper(files), "GSE[[:digit:]]*")) %>% 
+    mutate(Accession = str_extract(toupper(files), acc)) %>% 
     select(Accession, everything())
   
   # Unique series matrix files
@@ -28,14 +29,15 @@ munge_matrixfiles <- function(out_path) {
       getGEO(filename = path, getGPL = FALSE)
     }
   )
-  
+  gsm <- str_c(acc, "(-GPL[[:digit:]]+)?_series_matrix.txt.gz")
   weird_stuff <- matrixfiles %>% 
-    filter(!str_detect(series_matrix_file, "GSE[0-9]*(-GPL[0-9]*)?_series_matrix.txt.gz"))
+    filter(!str_detect(basename(series_matrix_file), gsm))
   
   gsem <- matrixfiles %>%
-    filter(str_detect(series_matrix_file, "GSE[0-9]*(-GPL[0-9]*)?_series_matrix.txt.gz")) %>% 
+    filter(str_detect(basename(series_matrix_file), gsm)) %>% 
     mutate(gse = map(series_matrix_file, safely_getGEO))
-  
+
+  write_csv(weird_stuff, path = file.path(dirname(out_path), "not_real_series_matrix.csv"))
   write_rds(gsem, path = out_path)
 }
 
