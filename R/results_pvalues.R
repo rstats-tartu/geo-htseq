631e8f4f7e21a7a91e815ea8ac02103109a3a97f
Taavi Päll <taavi74@ut.ee>
2019-02-19 13:30:15 +0000

    updated type conversion rate

--- R/results_pvalues.R
+++ R/results_pvalues.R
@@ -307,68 +307,25 @@ spark_table_bm <- p_values_bm %>%
                                 chartRangeMin = 0,
                                 type = "bar"))
 
-# Curated p value histogram classes ---------------------------------------
+#' Curated p value histogram classes ---------------------------------------
 
 
-# Histogram types summary table
-#types_legend <- read_csv("data/pvalue_hist_types.csv")
+#' ## Read in classifications
+nnet_classes <- read_csv(here("output/pvalue_histogram_nnet_classification.csv")) 
+nnet_classes_spread <- nnet_classes %>% 
+  spread(Filter, Type) %>% 
+  select(suppdata_id, Type = raw, `Type filtered` = basemean, Method)
 
-# Load manually assigned classes
-his_all <- read_delim(here("data/pvalue_hist_UM_190121.csv"), 
-                      delim = ",", 
-                      locale = locale(decimal_mark = ","))
-# Code used to identify rows for fixing in data/pvalue_hist_UM_190121.csv
-# his_all_to_be_updated <- bind_rows(p_values %>% 
-#             filter(suppfiles %in% his_all$`Supplementary file name`, suppfiles != suppdata_id) %>% 
-#             select(suppfiles, suppdata_id, pi0),
-#           p_values_bm %>% 
-#             filter(suppfiles %in% his_all$`Supplementary file name`, suppfiles != suppdata_id) %>% 
-#             select(suppfiles, suppdata_id, pi0), .id = "id")
-
-
-# Split manualy assigned classes to filtered and non-filtered
-his_bm <- his_all %>%
-  select(Accession,
-         suppdata_id = `Supplementary file name`,
-         pi0 = `True nulls proportion filtered`,
-         Type = `Type filtered`) %>%
-  filter(!is.na(Type))
-
-his <- his_all %>%
-  select(Accession,
-         suppdata_id = `Supplementary file name`,
-         pi0 = `True nulls proportion`,
-         Type) %>%
-  filter(!is.na(Type))
-
-# Read in classifications from random forest.
-his_pre <- read_csv(here("data/histogram_classification_prefiltration.csv")) 
-
-his_post <- read_csv(here("data/histogram_classification_postfiltration.csv")) 
-
-# combine classifications and rename low pi0 to all_effects
-#his <- full_join(distinct(his),distinct(his_pre)) %>%
-#  mutate(Type = replace(Type,as.numeric(pi0) <= pi0threshold, "all_effects"))
-#his_bm <- full_join(distinct(his_bm),distinct(his_post)) %>%
-#  mutate(Type = replace(Type,as.numeric(pi0) <= pi0threshold, "all_effects"))
-
-# Module to use only classification from random forest.
-his <- his_pre
-his_bm <- his_post
-
-# Merge with classes and generate output table
+#' Merge with classes and generate output table
 spark_table <- spark_table %>%
   ungroup() %>% 
-  left_join(select(his, -pi0)) %>%
-  arrange(Accession) %>% 
-  distinct() %>%
+  left_join(nnet_classes_spread) %>%
   rename('P value histogram' = Histogram,
          'True nulls proportion' = pi0,
          'Supplementary file name' = suppdata_id) %>% 
-  arrange(Type) %>%
-  filter(!map_int(Type,is.na)) #if you want to show only classified stuff
+  filter(!map_int(Type, is.na)) # if you want to show only classified stuff
 
-# Save empty table for manual classification. 
+#' Save empty table for manual classification. 
 if (!any(str_detect(list.files(here("output")), "pvalue_histogram_classes.csv"))) {
   spark_table %>% 
     select(-`P value histogram`) %>% 
@@ -376,32 +333,21 @@ if (!any(str_detect(list.files(here("output")), "pvalue_histogram_classes.csv"))
 }
 
 hist_types <- spark_table %>% 
-  group_by(Type) %>% 
-  summarise(N = n()) %>%
-  ungroup() %>% 
-  mutate(`%` = percent(N / sum(N), digits = 1))
+  count(Type) %>%
+  mutate(`%` = percent(n / sum(n), digits = 1))
 
-# Recalculated frequencies after basemean filtering
-# Does nto work properly as for the same suppdata_id can be several sets which will get combined multiple times,
-# therefore increasing number of rows used for comparision. It means that all combinations from the same set will be 
-# compared.
-type_conversion_rate <- 
-  left_join(
-  select(spark_table, Accession, `Supplementary file name`, Type), 
-  select(his_bm, Accession, `Supplementary file name` = suppdata_id, type_after_filter = Type)
-  ) %>% 
-  filter(!is.na(type_after_filter)) %>% 
+#' Recalculated frequencies after basemean filtering
+type_conversion_rate <- spark_table %>% 
+  filter(complete.cases(.)) %>% 
   mutate(type_conversion = case_when(
-    type_after_filter == Type ~ "same",
-    type_after_filter != Type & type_after_filter == "anti-conservative" ~ "improvement",
-    (type_after_filter != "uniform" || type_after_filter != "anti-conservative") & Type == "anti-conservative" ~ "worse",
-    type_after_filter == "uniform" & Type == "anti-conservative" ~ "effects were lost",
+    `Type filtered` == Type ~ "same",
+    `Type filtered` != Type & `Type filtered` == "anti-conservative" ~ "improvement",
+    (`Type filtered` != "uniform" || `Type filtered` != "anti-conservative") & Type == "anti-conservative" ~ "worse",
+    `Type filtered` == "uniform" & Type == "anti-conservative" ~ "effects were lost",
     TRUE ~ "no improvement"
   )) %>% 
-  group_by(type_conversion) %>% 
-  summarise(N = n()) %>%
-  ungroup() %>% 
-  mutate(`%` = percent(N / sum(N), digits = 1))
+  count(type_conversion) %>% 
+  mutate(`%` = percent(n / sum(n), digits = 1))
 
 # Merge with classes and generate output table
 spark_table_bm_renamed <- spark_table_bm %>%
