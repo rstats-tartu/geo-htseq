e7a033a0cef8d0a9fc1b2e05e08f48763cdd19c8
Taavi Päll <taavi74@ut.ee>
2019-08-21 16:20:13 +0300

    use snakemake to run download publications script

--- R/07_download_publications.R
+++ R/07_download_publications.R
@@ -4,7 +4,8 @@ source("R/_common.R")
 library(entrezquery)
 
 # Check if document summary table is already loaded
-ds <- read_rds("output/document_summaries.rds")
+message("Loading document summaries")
+ds <- read_rds(snakemake@input[[1]])
 
 # All HT-seq datasets
 # Lump together all non-human and murine taxa
@@ -15,18 +16,18 @@ fix_pmid <- function(x) {
     str_split(" ") %>% 
     unlist()
 }
-
-# Download publications for all taxa
+message("Filtering publications by last date")
 ds_pmids <- ds %>%
-  filter(PDAT <= last_date, 
+  filter(PDAT <= snakemake@params[["last_date"]], 
          str_length(PubMedIds) != 0) %>% 
   select(PubMedIds) %>% 
   mutate(PubMedIds = map(PubMedIds, fix_pmid)) %>% 
   unnest() %>% 
   distinct()
 
+message("Downloading publications for all taxa")
 publications <- ds_pmids$PubMedIds %>% 
   entrez_docsums(uid = ., db = "pubmed", wait = 0.33)
 publications <- publications %>% 
   unnest()
-write_csv(publications, "output/publications.csv")
+write_csv(publications, snakemake@output[[1]])
