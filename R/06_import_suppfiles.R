24b930d018a548cfd6da88165550ba7fd58a9699
Taavi Päll <taavi74@ut.ee>
2019-08-21 16:18:54 +0300

    exclude some more bad files that crash R

--- R/06_import_suppfiles.R
+++ R/06_import_suppfiles.R
@@ -9,6 +9,8 @@ library(GEOquery)
 
 import_suppdata <- function(supptab, out_path) {
   supptab <- read_rds(supptab)
+  bad <- c("GSE121228_gene_expression_anno.txt.gz", "GSE113074_Raw_combined.annotated_counts.tsv.gz", "GSE113074_Corrected_combined.annotated_counts.tsv.gz", "GSE121737_early_and_medium_bud.repGene.txt.gz")
+  supptab <- supptab %>% filter(!(suppfiles %in% bad))
   supptab %>%
     mutate(result = map(suppfiles, ~ try(munge_geo_pvalue(file.path(local_suppfile_folder, .x))))) %>% 
     write_rds(path = out_path)
