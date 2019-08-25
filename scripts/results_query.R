b0e256ef1e0b959612648b0bb1b7f50bd202c4a1
Taavi Päll <taavi74@ut.ee>
2019-08-25 14:18:38 +0000

    added yearly geo growth

--- scripts/results_query.R
+++ scripts/results_query.R
@@ -7,7 +7,7 @@ source("scripts/_common.R")
 ## ---- rna-seq-dynamics ----
 
 # R/A01_GEO_query.R
-ds <- read_rds("output/document_summaries.rds") # GEO HT-seq expr datasets
+ds <- read_csv("output/document_summaries.csv") # GEO HT-seq expr datasets
 
 # Date of the first submission
 first_date <- range(ymd(ds$PDAT))[1]
@@ -27,7 +27,7 @@ write_csv(ds_redline, "output/ds_redline.csv")
 
 # Count series with publications
 pdat <- ds_redline %>% 
-  mutate(pub = str_length(PubMedIds) != 0) %>% 
+  mutate(pub = !is.na(PubMedIds)) %>% 
   group_by(model, PDAT) %>% 
   summarise(geoseries = n(), 
             pub = sum(pub)) %>% 
@@ -42,6 +42,25 @@ geop <- pdat %>%
   labs(x = "Publication date", 
        y = "Number of GEO series") +
   scale_linetype_discrete(labels = c("All series","Series with\npublications")) +
+  theme(legend.position = c(0.75, 0.77),
+        axis.text.x = element_text(angle = 45, hjust = 1),
+        legend.background = element_blank(),
+        legend.key = element_blank(),
+        legend.title = element_blank())
+
+# Yearly submissions
+geop_year <- ds_redline %>% 
+  mutate(pub = !is.na(PubMedIds)) %>% 
+  group_by(model, year = lubridate::year(PDAT)) %>% 
+  summarise(geoseries = n(), 
+            pub = sum(pub)) %>% 
+  mutate(ppub = pub / geoseries) %>% 
+  ggplot(aes(x = year, y = geoseries)) +
+  geom_bar(stat = "identity") +
+  scale_x_continuous(breaks = seq(2006, 2018, by = 2)) +
+  facet_wrap(~model) +
+  labs(x = "Publication year", 
+       y = "Number of GEO series") +
   theme(legend.position = c(0.75, 0.77),
         axis.text.x = element_text(angle = 45, hjust = 1),
         legend.background = element_blank(),
@@ -74,5 +93,9 @@ ppub_n_ci <- ppub_n %>%
          ci = map(ci, percent, 1),
          ci = map_chr(ci, ~ glue("95%CI, {.x[1]} to {.x[2]}")))
 
+pg <- lapply(list(geop_year, geop), ggplotGrob)
+pg <- add_labels(pg, case = panel_label_case)
+query_fig <- arrangeGrob(grobs = pg, nrow = length(pg))
+
 ## ---- queryfig -----
-geop
+grid.draw(query_fig)
