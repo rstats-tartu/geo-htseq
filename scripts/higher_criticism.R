24f4e66f76d98418db6dedd6b14a6ecec9fb208b
Taavi Päll <taavi74@ut.ee>
2019-11-12 17:04:37 +0000

    precalculate breaks

--- scripts/higher_criticism.R
+++ scripts/higher_criticism.R
@@ -1,11 +1,12 @@
 library(tidyverse)
 library(here)
 
+megatab <- read_csv(here("output/megatab.csv"))
 pvalues <- read_rds(here("output/pvalues.rds"))
-higher_criticism <- function(x, breaks = 20) {
+higher_criticism <- function(x, breaks) {
   x <- na.omit(x)
-  counts <- hist(x, breaks = breaks, plot = FALSE)$counts
-  b <- 0.05
+  counts <- hist(x, breaks = seq(0, 1, length.out = breaks), plot = FALSE)$counts
+  b <- 1 / breaks
   qc <- qbinom(1 - b * 0.05, length(x), b)
   counts_over_qc <- counts >= qc
   end_of_peak <- rle(diff(counts_over_qc))$lengths[1] + 1
@@ -13,18 +14,20 @@ higher_criticism <- function(x, breaks = 20) {
 }
 
 safe_HC <- safely(higher_criticism)
+
 pvalues_qc <- pvalues %>% 
   select(suppdata_id, features, pvalues) %>% 
-  mutate(HC = map(pvalues, safe_HC, breaks = 42))
+  mutate(HC = map(pvalues, safe_HC, breaks = 40))
 pvalues_qc_pass <- pvalues_qc %>% 
   mutate(QC_pass = map(HC, "result")) %>% 
   filter(!map_lgl(QC_pass, is.null)) %>% 
   unnest(QC_pass) %>% 
   select(suppdata_id, QC_pass)
 
-megatab <- read_csv(here("output/megatab.csv"))
 megatab_qc <- megatab %>% 
-  left_join(pvalues_qc_pass) %>% 
+  inner_join(pvalues_qc_pass) %>% 
   select(suppdata_id, Type, pi0, QC_pass)
 megatab_qc %>%
-  count(Type, QC_pass)
+  count(Type, QC_pass) %>% 
+  spread(QC_pass, n) %>% 
+  mutate(`%` = formattable::percent(`TRUE` / (`FALSE` + `TRUE`)))
