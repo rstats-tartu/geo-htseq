7ac330ebda7bf521350f69b441f40100b1a207a9
Taavi Päll <taavi74@ut.ee>
2019-11-12 17:35:45 +0000

    added fdr parameter

--- scripts/higher_criticism.R
+++ scripts/higher_criticism.R
@@ -3,11 +3,11 @@ library(here)
 
 megatab <- read_csv(here("output/megatab.csv"))
 pvalues <- read_rds(here("output/pvalues.rds"))
-higher_criticism <- function(x, breaks) {
+higher_criticism <- function(x, breaks, fdr) {
   x <- na.omit(x)
   counts <- hist(x, breaks = seq(0, 1, length.out = breaks), plot = FALSE)$counts
   b <- 1 / breaks
-  qc <- qbinom(1 - b * 0.05, length(x), b)
+  qc <- qbinom(1 - b * fdr, length(x), b)
   counts_over_qc <- counts >= qc
   end_of_peak <- rle(diff(counts_over_qc))$lengths[1] + 1
   !any(counts_over_qc[end_of_peak:length(counts_over_qc)])
@@ -17,7 +17,7 @@ safe_HC <- safely(higher_criticism)
 
 pvalues_qc <- pvalues %>% 
   select(suppdata_id, features, pvalues) %>% 
-  mutate(HC = map(pvalues, safe_HC, breaks = 40))
+  mutate(HC = map(pvalues, safe_HC, breaks = 40, fdr = 0.05))
 pvalues_qc_pass <- pvalues_qc %>% 
   mutate(QC_pass = map(HC, "result")) %>% 
   filter(!map_lgl(QC_pass, is.null)) %>% 
