d39cf325cfd61d61851f5e3ff7acbc611ce0dfe5
ymaivali <ymaivali@gmail.com>
2019-11-12 19:57:57 +0200

    added plots

--- scripts/higher_criticism.R
+++ scripts/higher_criticism.R
@@ -31,3 +31,23 @@ megatab_qc %>%
   count(Type, QC_pass) %>% 
   spread(QC_pass, n) %>% 
   mutate(`%` = formattable::percent(`TRUE` / (`FALSE` + `TRUE`)))
+
+qc_plot <- function(x, breaks) {
+  x <- na.omit(x)
+  b <- 1 / breaks
+  qc <- qbinom(1 - b * 0.05, length(x), b)
+  ggplot(data = NULL) +
+    geom_histogram(aes(x), bins = breaks) +
+    geom_hline(yintercept = qc, linetype = "dashed") +
+    theme(axis.title = element_blank())
+}
+
+pvalues_qc_plots <- pvalues_qc %>% 
+  mutate(QC_pass = map(HC, "result")) %>% 
+  filter(!map_lgl(QC_pass, is.null)) %>% 
+  unnest(QC_pass) %>% 
+  select(-HC) %>% 
+  mutate(p = map(pvalues, qc_plot, breaks = 40))
+
+pvalues_qc_plots %>% 
+  mutate(p_saved = map2(p, suppdata_id, ~ggsave(.x, filename = here("output/plots", str_c(str_extract(.y, "\\w+"), ".png")), width = 2, height = 1.34)))
