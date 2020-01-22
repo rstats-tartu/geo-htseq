103273d57d2e33c078eae2381a74ea7ea2a21d15
Taavi Päll <tapa741@gmail.com>
2020-01-22 15:11:18 +0200

    import xls.gz file

--- scripts/import_suppfiles.py
+++ scripts/import_suppfiles.py
@@ -1,33 +1,44 @@
 import pandas as pd
 import os
 import re
+import gzip
 
-p = re.compile("p.*val")
+pv = re.compile("p.*val")
+sp = re.compile("\s+")
 
-path = "output/suppl/"
-suppfiles = os.listdir(path)
+dir = "output/suppl/"
+suppfiles = os.listdir(dir)
 
 for input in suppfiles:
     print("Working on: ", input)
+    path = dir + input
     tabs = {}
     if "xls" in input:
         try:
-            wb = pd.ExcelFile(path + input)
+            if ".gz" in input:
+                with gzip.open(path) as gz:
+                    wb = pd.ExcelFile(gz)
+            else:
+                wb = pd.ExcelFile(path)
             sheets = wb.sheet_names
             for sheet in sheets:
-                tabs.update({input + "_" + sheet : wb.parse(sheet, engine="xlrd")})
+                parsed_sheet = wb.parse(sheet)
+                if not parsed_sheet.empty:
+                    tabs.update({input + "-sheet-" + sheet: parsed_sheet})
         except Exception as e:
             print(input, " file: ", e)
             pass
     else:
         try:
-            r = pd.read_csv(path + input, sep=None, engine='python', iterator=True, skiprows = 20, nrows = 1000)
+            r = pd.read_csv(
+                path, sep=None, engine="python", iterator=True, skiprows=20, nrows=1000
+            )
             sep = r._engine.data.dialect.delimiter
             r.close()
-            tabs.update({input : pd.read_csv(path + input, sep=sep, comment="#")})
+            tabs.update({input: pd.read_csv(path, sep=sep, comment="#")})
         except Exception as e:
             print("Error: ", e)
             pass
-    for k,v in tabs.items():
+    for k, v in tabs.items():
         print("Table: ", k)
         print(v)
