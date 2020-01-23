7416a943d07513cc6ae021031b0a82e8f0ec7176
Taavi Päll <tapa741@gmail.com>
2020-01-23 17:17:57 +0200

    moved import to separate function

--- scripts/import_suppfiles.py
+++ scripts/import_suppfiles.py
@@ -2,31 +2,29 @@ import pandas as pd
 import os
 import re
 import gzip
+import tarfile
 
 pv = re.compile("p.*val")
 sp = re.compile("\s+")
+xls = re.compile("xls")
 
-dir = "output/suppl/"
-suppfiles = os.listdir(dir)
 
-for input in suppfiles:
-    print("Working on: ", input)
-    path = dir + input
+def import_tabs(path):
     tabs = {}
-    if "xls" in input:
+    if xls.search(path):
         try:
-            if ".gz" in input:
+            if path.endswith(".gz"):
                 with gzip.open(path) as gz:
                     wb = pd.ExcelFile(gz)
             else:
                 wb = pd.ExcelFile(path)
             sheets = wb.sheet_names
             for sheet in sheets:
-                parsed_sheet = wb.parse(sheet)
-                if not parsed_sheet.empty:
-                    tabs.update({input + "-sheet-" + sheet: parsed_sheet})
+                df = wb.parse(sheet, comment="#")
+                if not df.empty:
+                    tabs.update({os.path.basename(path) + "-sheet-" + sheet: df})
         except Exception as e:
-            print(input, " file: ", e)
+            print("Error: ", e)
             pass
     else:
         try:
@@ -34,11 +32,36 @@ for input in suppfiles:
                 path, sep=None, engine="python", iterator=True, skiprows=20, nrows=1000
             )
             sep = r._engine.data.dialect.delimiter
+            df = pd.read_csv(path, sep=sep, comment="#")
+            if all(["Unnamed" in i for i in list(df.columns)]):
+                idx = find_header(df)
+                if idx > 0:
+                    df = pd.read_csv(path, sep=sep, comment="#", skiprows=idx)
+            tabs.update({input: df})
             r.close()
-            tabs.update({input: pd.read_csv(path, sep=sep, comment="#")})
         except Exception as e:
             print("Error: ", e)
             pass
+    return tabs
+
+
+def find_header(df, n=20):
+    head = df.head(n)
+    idx = 0
+    for index, row in head.iterrows():
+        if all([isinstance(i, str) for i in row]):
+            idx = index
+            break
+    return idx
+
+
+dir = "output/suppl/"
+suppfiles = os.listdir(dir)
+
+for input in suppfiles:
+    print("Working on: ", input)
+    path = dir + input
+    tabs = import_tabs(path)
     for k, v in tabs.items():
         print("Table: ", k)
-        print(v)
+        print(v.head())
