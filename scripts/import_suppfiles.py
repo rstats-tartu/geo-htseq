e2f8637ab9d5341365733e112492facc841db84b
Taavi Päll <tapa741@gmail.com>
2020-01-24 17:09:46 +0200

    import works with flat files and archives

--- scripts/import_suppfiles.py
+++ scripts/import_suppfiles.py
@@ -3,47 +3,15 @@ import os
 import re
 import gzip
 import tarfile
+import io
 
-pv = re.compile("p.*val")
-sp = re.compile("\s+")
 xls = re.compile("xls")
-
-
-def import_tabs(path):
-    tabs = {}
-    if xls.search(path):
-        try:
-            if path.endswith(".gz"):
-                with gzip.open(path) as gz:
-                    wb = pd.ExcelFile(gz)
-            else:
-                wb = pd.ExcelFile(path)
-            sheets = wb.sheet_names
-            for sheet in sheets:
-                df = wb.parse(sheet, comment="#")
-                if not df.empty:
-                    tabs.update({os.path.basename(path) + "-sheet-" + sheet: df})
-        except Exception as e:
-            print("Error: ", e)
-            pass
-    else:
-        try:
-            r = pd.read_csv(
-                path, sep=None, engine="python", iterator=True, skiprows=20, nrows=1000
-            )
-            sep = r._engine.data.dialect.delimiter
-            df = pd.read_csv(path, sep=sep, comment="#")
-            if all(["Unnamed" in i for i in list(df.columns)]):
-                idx = find_header(df)
-                if idx > 0:
-                    df = pd.read_csv(path, sep=sep, comment="#", skiprows=idx)
-            tabs.update({input: df})
-            r.close()
-        except Exception as e:
-            print("Error: ", e)
-            pass
-    return tabs
-
+keep = "|".join(
+    ["\." + i + "(.gz)?$" for i in "tab xlsx diff tsv xls csv txt rtf".split(" ")]
+)
+keep = re.compile(keep)
+gse = re.compile("GSE\d+_")
+pv = re.compile("p.*val")
 
 def find_header(df, n=20):
     head = df.head(n)
@@ -55,13 +23,88 @@ def find_header(df, n=20):
     return idx
 
 
+def read_csv(input, tar=None):
+    input_name = input
+    csv = input
+    if isinstance(input, (tarfile.TarInfo)):
+        input_name = gse.search(tar.name).group(0) + input.name.replace("/", "_")
+        csv = io.StringIO(tar.extractfile(input).read().decode("unicode_escape"))
+        input = tar.extractfile(input)
+    r = pd.read_csv(
+        csv, sep=None, engine="python", iterator=True, skiprows=20, nrows=1000
+    )
+    sep = r._engine.data.dialect.delimiter
+    df = pd.read_csv(input, sep=sep, comment="#", encoding="unicode_escape")
+    if all(["Unnamed" in i for i in list(df.columns)]):
+        idx = find_header(df)
+        if idx > 0:
+            df = pd.read_csv(
+                input, sep=sep, comment="#", skiprows=idx, encoding="unicode_escape"
+            )
+    return {os.path.basename(input_name): df}
+
+
+def read_excel(input, tar=None):
+    tabs = {}
+    input_name = input
+    if isinstance(input, (tarfile.TarInfo)):
+        input_name = gse.search(tar.name).group(0) + input.name.replace("/", "_")
+        input = tar.extractfile(input)
+    if input_name.endswith(".gz"):
+        with gzip.open(input) as gz:
+            wb = pd.ExcelFile(gz)
+    else:
+        wb = pd.ExcelFile(input)
+    sheets = wb.sheet_names
+    for sheet in sheets:
+        df = wb.parse(sheet, comment="#")
+        if not df.empty:
+            tabs.update({os.path.basename(input_name) + "-sheet-" + sheet: df})
+    return tabs
+
+
+def import_tar(path):
+    out = {}
+    with tarfile.open(path, "r:*") as tar:
+        for member in tar:
+            if member.isfile():
+                if keep.search(member.name):
+                    if not member.name.startswith("."):
+                        try:
+                            if xls.search(member.name):
+                                out.update(read_excel(member, tar))
+                            else:
+                                out.update(read_csv(member, tar))
+                        except Exception as e:
+                            print("Error: ", e)
+    return out
+
+
+def import_flat(path):
+    out = {}
+    try:
+        if xls.search(path):
+            out.update(read_excel(path))
+        else:
+            out.update(read_csv(path))
+    except Exception as e:
+        print("Error: ", e)
+    return out
+
+
 dir = "output/suppl/"
 suppfiles = os.listdir(dir)
+# path = "/Users/taavi/Downloads/GSE0_test.tar.gz"
 
+out = {}
 for input in suppfiles:
     print("Working on: ", input)
     path = dir + input
-    tabs = import_tabs(path)
-    for k, v in tabs.items():
-        print("Table: ", k)
-        print(v.head())
+    if path.endswith("tar.gz"):
+        out.update(import_tar(path))
+    else:
+        out.update(import_flat(path))
+
+for k, v in out.items():
+    print("Table: ", k)
+    print(v.head())
