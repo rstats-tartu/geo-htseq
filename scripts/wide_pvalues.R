84dc8d191bfb4afdbe15d34a214a9ca4a4682313
Taavi Päll <tapa741@gmail.com>
2020-02-14 18:21:47 +0200

    added pivo wide script

---
+++ scripts/wide_pvalues.R
@@ -0,0 +1,31 @@
+library(tidyverse)
+library(sparkline)
+library(formattable)
+
+#' Import dataset
+imported <- read_csv("output/parsed_suppfiles.csv")
+imported <- imported %>% 
+  mutate(accession = str_extract(id, "GSE\\d+")) %>% 
+  select(accession, everything())
+
+#' Pivo wide
+pvalues <- imported %>% 
+  filter(!is.na(Type)) %>% 
+  mutate(Class = str_replace(Class, "anti-conservative", "anti-cons."),
+         Conversion = str_replace(Conversion, "improvement", "impr."))
+before <- pvalues %>% filter(Type == "raw")
+after <- pvalues %>% filter(Type != "raw") %>% 
+  rename_at(vars(Class, pi0, FDR_pval, hist), str_c, "_after") %>% 
+  rename(filter_var = Type)
+wide <- full_join(before, after) %>% 
+  select(accession, id, starts_with("class"), Conversion, starts_with("pi0"), starts_with("hist"), filter_var, Set)
+
+
+
+#' Generate html table
+tab <- wide %>% 
+  mutate_at(vars(starts_with("hist")), ~map(.x, ~as.integer(unlist(str_extract_all(.x, "\\d+"))))) %>% 
+  mutate_at(vars(starts_with("hist")), ~map(.x, spk_chr, type = "bar")) %>%
+  formattable() %>%
+  formattable::as.htmlwidget() %>%
+  spk_add_deps()
